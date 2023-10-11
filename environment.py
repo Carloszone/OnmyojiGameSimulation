4b2029131284dd131a4e06a10d426e0468f9a658
liuzhao.tang@svfactory.com.cn <ssFp@3985>
2023-10-11 18:01:44 +0800

    code upload

--- environment.py
+++ environment.py
@@ -1,34 +1,67 @@
 import numpy as np
 import random
 
-
-class shishen:
-    def __init__(self, name, hp, atk, defense, speed, crit, crit_damage, hit, resist):
-        self.name = name
-        self.hp = self.max_hp = hp
-        self.atk = atk
-        self.defense = defense
+class Judge:
+    def __init__(self, speed, round=0, buff_ratio=0.15, debuff_ratio=0.15):
+        self.id = 0
         self.speed = speed
-        self.crit = crit
-        self.crit_damage = crit_damage
-        self.hit = hit
-        self.resist = resist
-        self.state = {}
+        self.process = 0
+        self.time = np.round((1000-self.process) / speed,4)
+        self.round = round
+        self.buff_factor = buff_ratio * min(10, self.round)
+        self.debuff_factor = debuff_ratio * min(10, self.round)
 
-    def get_info(self, obj):
+    def action(self):
+        self.round += 1
+
+    def locate_info(self, att, n, reverse=False):
+        return [(self, self.time)]
+
+    def att_update(self, att, value):
+        setattr(self, att, )
+
+
+
+class Env:
+    def __init__(self, team1, team2, judge):
+        self.team1 = team1
+        self.team2 = team2
+        self.judge = judge
+        self.step_count = 0
+
+    def locate_actor(self):
+        actors = []
+        for obj in [self.judge, self.team1, self.team2]:
+            actors += obj.locate_info('time', 1)
+
+        actors = sorted(actors, key=lambda x: x[1])
+        return actors[0]
+
+    def update_process(self, actor, value):
+        for team in [self.team1.team, self.team2.team]:
+            for char in team:
+                if char.id != actor.id:
+                    char.process += np.round(min(value * char.speed + char.process, 1000), 4)
+                else:
+                    char.process = 0
+                char.time = np.round((1000 - char.process) / char.speed, 4)
+
+    def begainning_effect(self):
         pass
 
-class skill:
-    def __init__(self, character, target, skill_type, skill_effect, skill_effect_type):
-        self.character = character
-        self.target = target
-        self.skill_type = skill_type
-        self.skill_effect = skill_effect
-        self.skill_effect_type = skill_effect_type
+    def step(self):
+        actor, time = self.locate_actor()
+        self.update_process(actor, time) # 更新所有对象的进度条和时间属性
 
-    def get_info(self, obj):
-        pass
+        if self.step_count == 0:
+            self.begainning_effect() # 开局效果
 
-    def skill_effect(self):
-        pass
+        actor.action() # 执行行动
+
+        death_flag, death_num = self.death_check() # 死亡检查
+        if death_flag:
+            self.team1.death_effect(death_num)
+            self.team2.death_effect(death_num)
+
+
 
