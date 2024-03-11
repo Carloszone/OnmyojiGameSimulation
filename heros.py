688c62c6dc7e75c81e20ec1944e1df586903f4c9
liuzhao.tang@svfactory.com.cn <ssFp@3985>
2024-03-11 15:57:18 +0800

    add hero skills

--- heros.py
+++ heros.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class Character:
-    def __init__(self, name, health, attack, defense, speed):
+    def __init__(self, name, health, mana, attack, defense, speed):
         self.name = name
         self.health = health
+        self.mana = mana
         self.attack = attack
         self.defense = defense
         self.speed = speed
@@ -15,11 +16,29 @@ class Character:
             attr_value = getattr(self, attr_name)
             print(f'{attr_name}: {attr_value}')
 
-    def make_damage(self, target):
-        damage = self.attack - target.defense
+    def skill_1(self, target, coefficient=1):
+        skill_name = 'skill_1'
+        damage = self.attack * coefficient - target.defense
         target.health -= damage
-        print(f'{self.name} attacks {target.name}, dealing {damage} damage!')
+        print(f'{self.name} uses {skill_name} to attack {target.name}, dealing {damage} damage!')
         print(f'{target.name} has {target.health} health left!')
+
+    def skill_2(self, target, coefficient=1):
+        # s powerful attack
+        pass
+
+    def skill_3(self, target, coefficient=1):
+        # ultra skill with special effect
+        pass
+
+    def choose_skill(self, target):
+        if self.mana <= 1:
+            self.skill_1(target)
+        elif self.mana == 3:
+            self.skill_3(target)
+        else:
+            self.skill_2(target)
+
 
 class env:
     def __init__(self, player_1, player_2):
@@ -40,6 +59,8 @@ class env:
     def action_bar(self, length=1000):
         palyer_1_time = (length - self.player_1.current_location) / self.player_1.speed
         palyer_2_time = (length - self.player_2.current_location) / self.player_2.speed
+        print(f"{self.player_1.name}' action time is {palyer_1_time}")
+        print(f"{self.player_2.name}' action time is {palyer_2_time}")
 
         if palyer_1_time < palyer_2_time:
             self.player_1.current_location = 0
@@ -57,14 +78,77 @@ class env:
             round_count += 1
             print(f'***Round {round_count} starts!***')
             action_player, target_player = self.action_bar()
-            print(f'{action_player.name} is acting!')
-            action_player.make_damage(target_player)
+            print(f'{action_player.name} is acting! He has {action_player.mana} mana!')
+            action_player.choose_skill(target_player)
             print('\n')
 
 
 if __name__ == '__main__':
-    hero_1 = Character('Joker', 1000, 100, 50, 150)
-    hero_2 = Character('batman', 800, 150, 50, 200)
+    class Batman(Character):
+        def skill_1(self, target, coefficient=1.25):
+            skill_name = 'bat_punch'
+            damage = self.attack * coefficient - target.defense
+            target.health -= damage
+            print(f'{self.name} uses {skill_name} to attack {target.name}, dealing {damage} damage!')
+            print(f'{target.name} has {target.health} health left!')
+            self.mana += 1
+
+        def skill_2(self, target, coefficient=3):
+            skill_name = 'bat_kick'
+            damage = self.attack * coefficient - target.defense
+            target.health -= damage
+            print(f'{self.name} uses {skill_name} to attack {target.name}, dealing {damage} damage!')
+            print(f'{target.name} has {target.health} health left!')
+            self.mana -= 2
+
+        def skill_3(self, target, coefficient=2):
+            skill_name = 'bat_nature'
+            self.defense = self.defense * coefficient
+            self.attack = self.attack * coefficient
+            self.speed = self.speed * coefficient
+            print(f'{self.name} uses {skill_name} to enhance himself!')
+            print(f"{self.name}'s defense from {self.defense / coefficient} to {self.defense} !")
+            print(f"{self.name}'s attack from {self.attack / coefficient} to {self.attack} !")
+            print(f"{self.name}'s speed from {self.speed / coefficient} to {self.speed} !")
+            self.mana -= 3
+
+
+    class Joker(Character):
+        def skill_1(self, target, coefficient=0.7):
+            skill_name = "joker's trick"
+            for i in range(2):
+                damage = self.attack * coefficient - target.defense
+                target.health -= damage
+                print(f'{self.name} uses {skill_name} to attack {target.name}, dealing {damage} damage!')
+                print(f'{target.name} has {target.health} health left!')
+            self.mana += 1
+
+        def skill_2(self, target, coefficient=1.5):
+            skill_name = "joker's laugh"
+            damage = self.attack * coefficient - target.defense
+            target.health -= damage
+            print(f'{self.name} uses {skill_name} to attack {target.name}, dealing {damage} damage!')
+            print(f'{target.name} has {target.health} health left!')
+
+            self.mana += 1
+            print(f'{self.name} uses {skill_name} to gain an additional mana! Now he has {self.mana} mana!')
+            self.mana -= 2
+
+        def skill_3(self, target, coefficient=0.5):
+            skill_name = "joker's shadow"
+            # self.defense = self.defense * coefficient
+            # self.speed = self.speed * coefficient
+            self.attack = self.attack / coefficient
+            self.health = self.health / coefficient
+            print(f'{self.name} uses {skill_name} to enhance himself!')
+            # print(f"{self.name}'s defense from {self.defense / coefficient} to {self.defense} !")
+            # print(f"{self.name}'s speed from {self.speed / coefficient} to {self.speed} !")
+            print(f"{self.name}'s attack from {self.attack * coefficient} to {self.attack} !")
+            print(f"{self.name}'s health from {self.health * coefficient} to {self.health} !")
+            self.mana -= 3
+
+    hero_1 = Joker('Joker', 1000, 3, 100, 50, 150)
+    hero_2 = Batman('batman', 800, 3, 150, 50, 200)
 
     game = env(hero_1, hero_2)
     game.step()
