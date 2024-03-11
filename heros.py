ead99c28d83919f7dbf7f829388932cdd2fc10c1
liuzhao.tang@svfactory.com.cn <ssFp@3985>
2024-03-11 14:10:00 +0800

    simple action bar

--- heros.py
+++ heros.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 class Character:
-    def __init__(self, name, health, attack, defense):
+    def __init__(self, name, health, attack, defense, speed):
         self.name = name
         self.health = health
         self.attack = attack
         self.defense = defense
+        self.speed = speed
+        self.current_location = 0
 
     def details(self):
         print(f"{self.name}'s Attributions:")
@@ -35,26 +37,34 @@ class env:
             return 0
         return 1
 
+    def action_bar(self, length=1000):
+        palyer_1_time = (length - self.player_1.current_location) / self.player_1.speed
+        palyer_2_time = (length - self.player_2.current_location) / self.player_2.speed
+
+        if palyer_1_time < palyer_2_time:
+            self.player_1.current_location = 0
+            self.player_2.current_location += self.player_2.speed * palyer_1_time
+            return self.player_1, self.player_2
+        else:
+            self.player_2.current_location = 0
+            self.player_1.current_location += self.player_1.speed * palyer_2_time
+            return self.player_2, self.player_1
+
+
     def step(self):
         round_count = 0
         while self.death_check():
             round_count += 1
             print(f'***Round {round_count} starts!***')
-            play_list = [self.player_1, self.player_2]
-
-            if round_count % 2:
-                action_player = self.player_1
-                target_player = self.player_2
-            else:
-                action_player = self.player_2
-                target_player = self.player_1
+            action_player, target_player = self.action_bar()
+            print(f'{action_player.name} is acting!')
             action_player.make_damage(target_player)
             print('\n')
 
 
 if __name__ == '__main__':
-    hero_1 = Character('Joker', 100, 10, 5)
-    hero_2 = Character('batman', 80, 15, 5)
+    hero_1 = Character('Joker', 1000, 100, 50, 150)
+    hero_2 = Character('batman', 800, 150, 50, 200)
 
     game = env(hero_1, hero_2)
     game.step()
